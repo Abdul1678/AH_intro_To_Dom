b5d9d7ab1a233f895f7786360ff09e6674a92b63
Abdul1678 <abdulahihussein345@gmail.com>
2025-04-07 07:09:54 +0000

    Moved script.js to scripts folder and updated index.html to reflect new path

---
+++ script.js
@@ -0,0 +1,15 @@
+let titleElement = document.getElementById("title");
+console.log(titleElement);
+
+let heading = document.querySelector("h1");
+console.log(heading);
+
+let description = document.querySelector(".description");
+console.log(description);
+
+let changeTextButton = document.querySelector("#changeText");
+
+console.log(changeTextButton);
+
+let descriptions = document.querySelectorAll("p.description");
+console.log(descriptions);
